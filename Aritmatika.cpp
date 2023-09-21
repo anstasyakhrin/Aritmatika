c52f778a9dd89c7863890a694e6f1201470afc07
anstasyakhrin <144661194+anstasyakhrin@users.noreply.github.com>
2023-09-21 12:16:07 +0700

    Create Aritmatika.cpp

---
+++ Aritmatika.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+int a, b, c; 
+cout << "Program Penjumlahan Aritmatika \n"; 
+cout <<" masukan angka pertama:";
+cin>> a; 
+cout<< " masukan angka kedua:";
+cin>> b;
+c=a+b; 
+cout<<"hasil penjumlahan adalah; " << a + b <<endl;
+cout<<"hasil Pengurangan adalah; " << a - b <<endl;
+cout<<"hasil Perkalian adalah; " << a * b <<endl;
+cout<<"hasil Pembagian adalah; " << a / b <<endl;
+cout<<"hasil sisa bagi adalah; " << a % b <<endl;
+return 0; 
+}
